5ea9766aa9e544d7ea791eab6eca99ec27cddc76
Js Sanjay <mname8741@gmail.com>
2023-01-09 12:38:45 +0530

    final

--- project-quiz/serverMiddleware/api/index.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const userRouter=require("../routes/user.route")
-const quizRouter=require("../routes/quiz.route")
-const app=express()
-app.use(express.json())
-app.use("/users",userRouter)
-app.use("/quiz",quizRouter)
-
-module.exports=app
